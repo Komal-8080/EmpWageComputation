d7aefcfa1d783208937272b24168111a63baa131
Komal-8080 <shindeg163@gmail.com>
2020-09-30 23:36:42 +0530

    Total Salary for Full and Part time

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -5,15 +5,23 @@ user=$(whoami)
 echo " $greetings $user to EmployeeMaster"
 
 fullday=8
+partTime=4
 salaryPerHour=20
-empCheck=$((RANDOM%2))
+empCheck=$((RANDOM%3))
 if [ $empCheck -eq 1 ];
 then
 echo "*Present*"
 totalSalary=$((salaryPerHour*fullday))
-echo "TotalSalary is $totalSalary"
+echo "TotalSalary for FullTime is $totalSalary"
+elif [ $empCheck -eq 2 ];
+then
+echo "*Present*"
+totalSalary=$((salaryPerHour*partTime))
+echo "TotalSalary for PartTime is $totalSalary"
 else [ $empCheck -eq 0 ];
 echo "*Absent*"
+totalSalary=0
+echo "TotalSalary is $totalSalary"
 fi
 
 

1cf9793c56838e89dbbae2ae349d60cf98f6f9f8
Komal-8080 <shindeg163@gmail.com>
2020-09-30 20:54:43 +0530

    Employee Attendence

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
 greetings="WELCOME"
-user=$whoami
-echo " $greeting $user to EmployeeMaster"
+user=$(whoami)
+echo " $greetings $user to EmployeeMaster"
+
+empCheck=$((RANDOM%2))
+if [ $empCheck -eq 1 ];
+then
+echo "*Present*"
+else [ $empCheck -eq 0 ];
+echo "*Absent*"
+fi
+
 

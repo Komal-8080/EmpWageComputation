26f9925239ef113156fc562af3d26e525d9582b2
Komal-8080 <shindeg163@gmail.com>
2020-09-30 21:04:01 +0530

    Total Salary for Full day

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -4,10 +4,14 @@ greetings="WELCOME"
 user=$(whoami)
 echo " $greetings $user to EmployeeMaster"
 
+fullday=8
+salaryPerHour=20
 empCheck=$((RANDOM%2))
 if [ $empCheck -eq 1 ];
 then
 echo "*Present*"
+totalSalary=$((salaryPerHour*fullday))
+echo "TotalSalary is $totalSalary"
 else [ $empCheck -eq 0 ];
 echo "*Absent*"
 fi

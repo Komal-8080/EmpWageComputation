4eca5c78f8b28617f1a86b336a6a4be46e92c2b9
Komal-8080 <shindeg163@gmail.com>
2020-10-01 13:13:18 +0530

    Employee salary in hours or days

--- empWageComputation.sh
+++ empWageComputation.sh
@@ -7,18 +7,32 @@ echo " $greetings $user to EmployeeMaster"
 fullTime=1
 partTime=2
 salaryPerHour=20
-salaryPerMonth=20
+Month=20
+
+for totalWorkingHours in 100
+do
 empCheck=$((RANDOM%3))
 case $empCheck in
-$fullTime)
-echo "*Present*"
-workingHours=8 ;;
-$partTime)
-echo "*Present*" 
-workingHours=4 ;;
-*)
-echo "*Absent*"
-workingHours=0 ;;
+	$fullTime)
+	echo "*Present*"
+	workingHours=8 ;;
+	$partTime)
+	echo "*Present*" 
+	workingHours=4 ;;
+	*)
+	echo "*Absent*"
+	workingHours=0 ;;
 esac
-TotalSalary=$((workingHours*salaryPerHour*salaryPerMonth))
-echo "Total Salary is $TotalSalary"
+
+empWorkingHours=$((workingHours*salaryPerHour))
+if [ $empWorkingHours -eq 0 ];
+then
+echo "Employee Absent"
+elif [ $empWorkingHours -lt $totalWorkingHours ];
+then
+echo "Total Salary per month is $((Month*empWorkingHours))"
+else
+echo "Total Salary is $((salaryPerHour*totalWorkingHours))"
+fi
+done
+
